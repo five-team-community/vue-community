099f53cd34e5bf9d59a230f2ce086f2b21187833
muyuniris <muyuniris@163.com>
2019-11-26 16:00:06 +0800

    base

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="Login">
+    <div class="login-box">
+      <div class="title">
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name:"Login",
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
